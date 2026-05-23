0334285d08a6bceec995aa476abe829014dc9389
DangKimAn <dangkiman41005@gmail.com>
2026-05-23 14:48:32 +0700

    fix : lint:check runningBand

--- backend/src/system-log/system-log.controller.spec.ts
+++ backend/src/system-log/system-log.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SystemLogController } from './system-log.controller';
+import { SystemLogService } from './system-log.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UserAccessGaurd } from '../auth/guards/access.guard';
 
 describe('SystemLogController', () => {
   let controller: SystemLogController;
@@ -7,7 +10,21 @@ describe('SystemLogController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SystemLogController],
-    }).compile();
+      providers: [
+        {
+          provide: SystemLogService,
+          useValue: {
+            getAllLogs: jest.fn(),
+            toggleAnomaly: jest.fn(),
+          },
+        },
+      ],
+    })
+      .overrideGuard(JwtAuthGuard)
+      .useValue({ canActivate: jest.fn(() => true) })
+      .overrideGuard(UserAccessGaurd)
+      .useValue({ canActivate: jest.fn(() => true) })
+      .compile();
 
     controller = module.get<SystemLogController>(SystemLogController);
   });
